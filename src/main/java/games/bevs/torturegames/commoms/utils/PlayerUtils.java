8424a913238d8b1f6d931060b557bcd5de2ab7f6
Heath Logan Campbell <github@heathlogancampbell.com>
2019-10-05 22:49:23 +1300

    fix

--- src/main/java/games/bevs/torturegames/commoms/utils/PlayerUtils.java
+++ src/main/java/games/bevs/torturegames/commoms/utils/PlayerUtils.java
@@ -1,5 +1,6 @@
 package games.bevs.torturegames.commoms.utils;
 
+import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.PlayerInventory;
@@ -10,7 +11,7 @@ public class PlayerUtils
 	{
 		PlayerInventory inv = player.getInventory();
 		for(ItemStack item : inv.getContents())
-			if(item != null)
+			if(item != null && item.getType() != Material.AIR)
 				player.getWorld().dropItemNaturally(player.getLocation(), item);
 		
 		for(ItemStack item : inv.getArmorContents())
